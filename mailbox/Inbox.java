672d2cbc285164e42d3443d2bd930d6fb282f7ae
DrewEdgette <49006910+DrewEdgette@users.noreply.github.com>
2020-07-12 14:29:12 -0400

    Add files via upload

--- mailbox/Inbox.java
+++ mailbox/Inbox.java
@@ -1,29 +1,33 @@
 package me.drew.mailbox;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.nio.charset.MalformedInputException;
 import java.util.UUID;
 
 public class Inbox {
-    private Map<UUID, ItemStack[]> inboxMap = new HashMap<>();
-    private String inboxName = "IDrinkMilk";
 
+    Mailbox plugin;
 
-    // gets inboxMap
-    public Map<UUID, ItemStack[]> getInboxMap() {
-        return inboxMap;
+    public Inbox(Mailbox plugin) {
+        this.plugin = plugin;
     }
 
-    // sets inboxMap
-    public void setInboxMap(Map<UUID, ItemStack[]> map) {
-        inboxMap = new HashMap<>(map);
+    private ItemStack[] inboxContents;
+    private String inboxName = "-";
+
+
+    // gets inbox contents
+    public ItemStack[] getInbox() {
+        return inboxContents;
     }
+
+    // sets inbox contents
+    public void setInbox(ItemStack[] contents) { inboxContents = contents; }
 
     // gets inboxName
     public String getInboxName() {
@@ -35,19 +39,57 @@ public class Inbox {
         inboxName = name;
     }
 
+    // opens the player's inbox
     public void openInbox(Player player) {
         inboxName = player.getDisplayName() + "'s Mailbox";
         Inventory inboxInv = Bukkit.createInventory(player, 36, inboxName);
 
-        if (inboxMap.get(player.getUniqueId()) != null) {
-            inboxInv.setContents(inboxMap.get(player.getUniqueId()));
+        if (plugin.getInboxItems(player.getUniqueId()) != null) {
+            inboxInv.setContents(plugin.getInboxItems(player.getUniqueId()));
         }
 
         player.openInventory(inboxInv);
     }
 
-    // tells you if a player owns an inbox or not
-    public boolean hasInbox(Player player) {
-        return (inboxName.contains(player.getDisplayName()));
+
+    // checks if a player owns an inbox
+    public boolean ownsInbox(UUID playerID, UUID skullID) {
+        return (playerID == skullID);
+    }
+
+
+    // determines what happens when a player clicks on a skull
+    public void clickSkull(Player player, UUID skullID) {
+        UUID playerID = player.getUniqueId();
+        ItemStack itemInHand = player.getInventory().getItemInMainHand();
+
+        // item has meta
+        if (itemInHand.hasItemMeta()) {
+            // holding sealed envelope
+            if (itemInHand.getItemMeta().getDisplayName().equals("Sealed Envelope")) {
+                player.sendMessage("you put the sealed envelope in the mailbox");
+                Inventory temp = Bukkit.createInventory(player, 36);
+                ItemStack[] items = plugin.getInboxItems(skullID);
+                temp.setContents(items);
+                temp.addItem(itemInHand);
+                items = temp.getContents();
+                plugin.setInboxItems(skullID, items);
+                player.getInventory().remove(itemInHand);
+            }
+            // not holding sealed envelope
+            else {
+                // player owns mailbox
+                if (ownsInbox(playerID, skullID)) {
+                    openInbox(player);
+                }
+            }
+        }
+        // item doesn't have meta
+        else {
+            // player owns mailbox
+            if (ownsInbox(playerID, skullID)) {
+                openInbox(player);
+            }
+        }
     }
 }
